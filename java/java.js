74aa241d760837a210923526bfbc0798da5970af
Chris <1995cbald@gmail.com>
2022-12-29 09:05:14 -0500

    Managed to collect form data for grid creation Bug: grid overpopulates when display is hit repeatedly Bug: Read box is not displaying correct values on display

--- java/java.js
+++ java/java.js
@@ -1,7 +1,8 @@
 let myLibrary = [];
-let container = document.getElementById("container");
+//let container = document.getElementById("container");
 var disp = document.querySelector("#button");
 var div = document.createElement('div');
+var sub = document.querySelector('#submit');
 
 
 
@@ -25,20 +26,17 @@ let headers = ['title', 'author', 'pages', 'read?']
 
 
 function addBookToLibrary(books) { 
-this.books = new Book(this.title, this.author, this.pages, this.read)
+this.books = new Book(this.title, this.author, this.pages, this.read) //added this. not sure it's a good idea...idk what im doing
 
     myLibrary.push(books);
 
 console.log(myLibrary) //no errors but only does console log which means results are transferring properly..should this access the prototype?
 }
 
-
-/*function display() {
-prompt("yay")
-} */
-
 let myTable = document.querySelector('#table');
 
+
+//displays current array in table
 disp.addEventListener("click", function() {
     let table = document.createElement('table');
     let headerRow = document.createElement('tr');
@@ -64,5 +62,43 @@ disp.addEventListener("click", function() {
 
     myTable.appendChild(table);
 })
+let container = document.querySelector("#container");
+const form = container.querySelectorAll('.form'),
+        submitInput = form[0].querySelector('input[type="submit"]') //can i include reset under clcik action submit?
+   // let submit = document.querySelector("#submit");
 
+
+function data() {
+
+let title = document.getElementById('title').value;
+console.log(title);
+
+let author = document.getElementById('author').value;
+console.log(author);
+
+let pages = document.getElementById('pages').value;
+console.log(pages);
+
+let read = document.getElementById('read').value;
+console.log(read);
+
+this.books = new Book(title, author, pages, read) 
+
+myLibrary.push(books);
+
+console.log(myLibrary)
+}
+
+/*
+submit.click(function () {
+    $("#form")[0].reset();
+    return false; // prevent submitting
+  }); */
     
+
+
+//problems that will need to be solved:
+//clearing form box after submission without erasing data
+//styling
+//grid building onitself. Need to reset when display is called
+//read box isn't presenting correct value
